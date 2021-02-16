abc6444e7acb6d36936350980adab47f6fb262ab
Heesoo Hwang <heesoohwang1231@gmail.com>
2021-02-16 13:33:56 -0800

    Fix Message
    Fixed the email message format

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"gopkg.in/gomail.v2"
 	"io/ioutil"
 	"strings"
+	"bufio"
 	"flag"
 )
 
@@ -20,12 +21,22 @@ func sendEmail(filename string) {
 		return
 	}
 	if (strings.Contains(string(result), "failure")) {
+		emailMessage := ""
+		CommitScanner := bufio.NewScanner(strings.NewReader(string(result)))
+		for CommitScanner.Scan() {
+			Message := CommitScanner.Text()
+			if (strings.Contains(Message, "┌─────────────────────────┬──────────────────┬──────────────────┐")) {
+				break
+			}
+			Message = Message + "\n"
+			emailMessage += Message
+		}
 		m := gomail.NewMessage()
 		m.SetHeader("From", "blockchainwarning@omnisolu.com")
 		m.SetHeader("To", "blockchainwarning@omnisolu.com")
 		//m.SetAddressHeader("Cc", "dan@example.com", "Dan")
 		m.SetHeader("Subject", "Dappley Web Block Check:")
-		m.SetBody("text/html", "<p> There is a failing test <br> Detailed inormation can be accessed throught the attachment file below. <br> NOTE*: Please make sure to download the file before opening it. </p>")
+		m.SetBody("text", emailMessage)
 		m.Attach(filename)
 
 		d := gomail.NewDialer("smtp.gmail.com", 587, "blockchainwarning@omnisolu.com", "01353751")
