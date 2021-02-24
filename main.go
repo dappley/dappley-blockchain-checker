49e5ea79cb6c3e2d970e1f0b7d0e3c110df6a38f
Heesoo Hwang <heesoohwang1231@gmail.com>
2021-02-23 16:52:41 -0800

    Daily Digest
    Code sends daily digest at 9AM when there hasn't been any error in past 24 hours

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"gopkg.in/gomail.v2"
 	"io/ioutil"
 	"strings"
+	"strconv"
 	"bufio"
+	"time"
 	"flag"
+	"log"
+	"os"
 )
 
 func main() {
@@ -16,11 +20,24 @@ func main() {
 }
 
 func sendEmail(filename string) {
+	timeStamp := time.Now().Unix()
+
 	result, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return
 	}
+
 	if (strings.Contains(string(result), "failure")) {
+		f, err := os.Create("lastError.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		_, err = f.WriteString(strconv.FormatInt(timeStamp, 10))
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		emailMessage := ""
 		CommitScanner := bufio.NewScanner(strings.NewReader(string(result)))
 		for CommitScanner.Scan() {
@@ -31,18 +48,75 @@ func sendEmail(filename string) {
 			Message = Message + "\n"
 			emailMessage += Message
 		}
-		m := gomail.NewMessage()
-		m.SetHeader("From", "blockchainwarning@omnisolu.com")
-		m.SetHeader("To", "blockchainwarning@omnisolu.com", "wulize1994@gmail.com", "rshi@omnisolu.com", "ilshiyi@omnisolu.com")
-		//m.SetAddressHeader("Cc", "dan@example.com", "Dan")
-		m.SetHeader("Subject", "***Important - Dappley Web Block Check:")
-		m.SetBody("text", emailMessage)
-		m.Attach(filename)
+		failEmail := gomail.NewMessage()
+		failEmail.SetHeader("From", "blockchainwarning@omnisolu.com")
+		failEmail.SetHeader("To", "blockchainwarning@omnisolu.com", 
+								  "wulize1994@gmail.com", 
+								  "rshi@omnisolu.com", 
+								  "ilshiyi@omnisolu.com")
+		//failEmail.SetAddressHeader("Cc", "dan@example.com", "Dan")
+		failEmail.SetHeader("Subject", "***Important - Dappley Web Block Check:")
+		failEmail.SetBody("text", emailMessage)
+		failEmail.Attach(filename)
 
 		d := gomail.NewDialer("smtp.gmail.com", 587, "blockchainwarning@omnisolu.com", "01353751")
 
-		if err := d.DialAndSend(m); err != nil {
+		if err := d.DialAndSend(failEmail); err != nil {
 			panic(err)
 		}
-	}
+	} else {
+		layout := "15:04:05"
+
+		lowerBound := "08:55:00"
+		before, err := time.Parse(layout, lowerBound)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		upperBound := "09:55:00"
+		after, err := time.Parse(layout, upperBound)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		curr := time.Now().Format("15:04:05")
+		now, err := time.Parse(layout, curr)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if (before.Before(now) && after.After(now)) {
+			data, err := ioutil.ReadFile("lastError.txt")
+			if err != nil {
+				log.Fatal(err)
+			}
+			lastTimeStamp, err := strconv.ParseInt(string(data), 10, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if (timeStamp - lastTimeStamp <= 86400) {  //if the time is 9AM
+				dailyDigest := gomail.NewMessage()
+				dailyDigest.SetHeader("From", "blockchainwarning@omnisolu.com")
+				dailyDigest.SetHeader("To", "blockchainwarning@omnisolu.com",
+											"wulize1994@gmail.com", 
+											"rshi@omnisolu.com", 
+											"ilshiyi@omnisolu.com")
+				dailyDigest.SetHeader("Subject", "Jenkins Daily Digest:")
+				emailMessage := ("Jenkins Daily Digest is sent every day at 9AM " + 
+								"when there hasn't been any error in last 24 Hours in dappley blockchain. " + 
+								"(http://dappley.dappworks.com/#/dappley/dashboard)\n\n"  +
+								"Detailed Info: \n	Last error:    " + 
+								time.Unix(lastTimeStamp, 0).String() + "\n	Lastest test: " + 
+								time.Unix(timeStamp, 0).String() + "\n\n▽ Lastest test result below ▽")
+				dailyDigest.SetBody("text", emailMessage)
+				dailyDigest.Attach(filename)
+	
+				d := gomail.NewDialer("smtp.gmail.com", 587, "blockchainwarning@omnisolu.com", "01353751")
+	
+				if err := d.DialAndSend(dailyDigest); err != nil {
+					panic(err)
+				}
+			}
+		}
+		}
 }
