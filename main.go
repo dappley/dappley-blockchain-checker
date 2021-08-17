a1950980df687acd0248e99b377e979130b3ee20
Heesoo Hwang <heesoohwang1231@gmail.com>
2021-08-17 13:57:54 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"log"
 )
 
 func main() {
@@ -13,11 +15,17 @@ func main() {
 
 	// Create flags
 	var email, passWord, main, test string
-	flag.StringVar(&email,    "email",    "default@example.com", "Email address of the sender")
+	flag.StringVar(&email,    "email",    "default_email", "Email address of the sender")
 	flag.StringVar(&passWord, "passWord", "default_password",    "Password of the sender email")
 	flag.StringVar(&test,     "test", "default_test.txt", "newman log file from http://3.16.250.102: Test Server.")
 	flag.StringVar(&main,     "main", "default_main.txt", "newman log file from http://dappley.dappworks.com: Main Server")
 	flag.Parse()
+
+	err := checkFlags(email, passWord, test, main)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
 	// Create the server test result email
 	fileNames := []string{main, test}
@@ -29,4 +37,21 @@ func main() {
 		sendEmail(subject, emailMessage, fileNames, email, passWord)
 		fmt.Println("Email sent!")
 	}
+}
+
+//----------Helper----------
+func checkFlags(email string, password string, test string, main string) (err error) {
+	switch {
+	case email == "default_email":
+		err = errors.New("Error: Email is missing!")
+	case password == "default_password":
+		err = errors.New("Error: Password is missing!")
+	case test == "default_test.txt":
+		err = errors.New("Error: Test server test result is missing!")
+	case main == "default_main.txt":
+		err = errors.New("Error: Main server test result is missing!")
+	default:
+		err = nil
+	}
+	return err
 }
